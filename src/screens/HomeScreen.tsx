f58f01b07e63178b50fb2dd5f3d06c9f0d5cba57
Jonathan Salinas <jonathan.salinas@digitalfemsa.com>
2024-12-07 17:59:24 -0600

    fix ts type

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,9 +2,13 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../navigation/types";
+
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<HomeScreenNavigationProp>();
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
